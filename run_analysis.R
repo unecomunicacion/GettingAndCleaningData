3563e1ee6dc0c85244b92febce6267f3d6769699
Une Comunicacion <une@unecomunicacion.es>
2015-02-21 11:05:22 +0100

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,6 @@
+require(knitr)
+require(markdown)
+# setwd("~/GitHub repositories/GettingAndCleaningData/Project")
+setwd("~/Repositories/GettingAndCleaningData/Project")
+knit("run_analysis.Rmd", encoding="ISO8859-1")
+markdownToHTML("run_analysis.md", "run_analysis.html")
